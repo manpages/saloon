8a8379931f5a5a809ad084fb37f0b6d42446ccc2
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-06-01 10:51:36 +0300

    nicer prepare/1

--- core/saloon/src/saloon_init.erl
+++ core/saloon/src/saloon_init.erl
@@ -13,5 +13,21 @@ prepare(Req) ->
 		true -> os:cmd("sh ./f5.sh"); %dirty-dirty
 		_ -> ok
 	end,
+	UID = case saloon_util:ck(<<"auth">>, Req) of 
+		undefined -> 0;
+		Cookie -> case saloon_auth:from_cookie(Cookie) of
+			UID2 when is_integer(UID2) -> UID2, io:format("Logged in!");
+			_ -> 0
+		end
+	end,
+	
+	Lang = case saloon_util:ck(<<"lang">>, Req) of
+		<<"lv">> -> lv;
+		<<"ru">> -> ru;
+		%% ...
+		_        -> en
+	end,
 
+	put(uid, UID),
+	put(ulang, Lang),
 	ok.
